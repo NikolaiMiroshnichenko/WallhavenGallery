3818aadaecf0f6379b51672c41847e7840e29296
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2020-07-17 18:03:01 +0300

    RestApiCLient update

--- WallhavenGallery/WallhavenGallery/Web/RestApiClient.cs
+++ WallhavenGallery/WallhavenGallery/Web/RestApiClient.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
+using System.Threading.Tasks;
+using WallhavenGallery.Models;
 
 namespace WallhavenGallery.Web
 {
@@ -9,10 +11,23 @@ namespace WallhavenGallery.Web
     {
         HttpClient httpClient = new HttpClient();
         private const string BaseUrl = "https://wallhaven.cc/api/v1/search?apikey=mgUbMpAaSerVpVSog4w5aTUdSl0OLxGI";
-        public async void webThings()
+        public async Task<IEnumerable<ImageModel>> GetAll()
         {
-            HttpResponseMessage response = httpClient.GetAsync()
-        }
+            try
+            {
+                var response = await httpClient.GetAsync(BaseUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var ImageModels = JsonConvert.DeserializeObject<IEnumerable<ImageModel>>(await response.Content.ReadAsStringAsync());
+                    return ImageModels;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return new List<ImageModel>();
+        }   
        
     }
 }
